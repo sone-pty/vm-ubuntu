ce77b23ca7bbdc403385ba51b4ba3eb3adfb72d5
sone-pty <921545242@qq.com>
2023-03-28 08:26:15 -0700

    main

--- main.cpp
+++ main.cpp
@@ -32,77 +32,9 @@
 using namespace json11;
 using namespace sone::utils;
 
-class Test
-{
-public:
-	Test() = default;
-
-	operator int() { return 1; }
-
-	void X(int i) 
-	{
-		std::lock_guard<std::mutex> lg(_mtx);
-		std::cout << "hello " << i << std::endl; 
-	}
-
-	void XX() 
-	{ 
-		std::lock_guard<std::mutex> lg(_mtx);
-		std::cout << "world" << std::endl; 
-	}
-
-	int XXX(int i) 
-	{ 
-		std::lock_guard<std::mutex> lg(_mtx);
-		return i + 10;
-	}
-
-private:
-	std::mutex _mtx;
-};
-
-template <typename RandomIter> int Parallel_Sum(const RandomIter& beg, const RandomIter& end)
-{
-	int sum = 0;
-
-	if(end - beg < 1000)
-	{
-		for(auto iter = beg; iter != end; ++iter)
-		{
-			sum += *iter;
-		}
-	}
-	else
-	{
-		RandomIter mid = beg + (end - beg) / 2;
-		std::future<int> res = std::async(std::launch::async, Parallel_Sum<RandomIter>, mid, end);
-		
-		for(auto iter = beg; iter != mid; ++iter)
-		{
-			sum += *iter;
-		}
-
-		sum += res.get();
-	}
-
-	return sum;
-}
-
-void test()
-{
-	uint32_t ping1 = sone::Sonenet::GetInstance()->NewService("ping");
-	uint32_t ping2 = sone::Sonenet::GetInstance()->NewService("ping");
-	uint32_t pong = sone::Sonenet::GetInstance()->NewService("pong");
-
-	auto msg1 = sone::Sonenet::GetInstance()->MakeMessage(ping1, "hi", 2);
-	auto msg2 = sone::Sonenet::GetInstance()->MakeMessage(ping2, "hello", 5);
-
-	sone::Sonenet::GetInstance()->Send(pong, msg1);
-	sone::Sonenet::GetInstance()->Send(pong, msg2);
-}
-
 off_t kRollSize = 500 * 1000 * 1000;
-sone::utils::AsyncLogging *g_asyncLog = NULL;
+AsyncLogging *g_asyncLog = NULL;
+
 void asyncOutput(const char *msg, int len)
 {
 	g_asyncLog->append(msg, len);
@@ -110,30 +42,13 @@ void asyncOutput(const char *msg, int len)
 
 int main(int argc, char *argv[])
 {
-	/* 	clock_t start = ::clock();
-		std::vector<int> lst(10000, 1);
-		int res = Parallel_Sum(lst.begin(), lst.end());
-		clock_t end = ::clock();
-		std::cout << "sum: " << res << ", cost: " << (end - start) << "ticks" << std::endl;
-
-		Test t;
-		int num = 10;
-		std::async(&Test::X, &t, num);
-		auto a2 = std::async(std::launch::deferred, &Test::XX, &t);
-		auto a3 = std::async(std::launch::async, &Test::XXX, &t, num);
-		a2.wait();
-		std::cout << "XXX(), num = " << a3.get() << std::endl; */
-
-	/* 	EventLoop loop;
-		loop.runAfter(2.0, [&loop]()->void{ loop.quit(); });
-		loop.loop(); */
-
+	// 日志线程
 	char name[256] = {'\0'};
 	strncpy(name, argv[0], sizeof name - 1);
-	sone::utils::AsyncLogging log(::basename(name), kRollSize);
+	AsyncLogging log(::basename(name), kRollSize);
 	log.start();
 	g_asyncLog = &log;
-	sone::utils::Logger::setOutput(asyncOutput);
-
+	Logger::setOutput(asyncOutput);
+	// 事件循环线程
 	return 0;
 }
